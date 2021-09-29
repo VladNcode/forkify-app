5a2e6baf316621a5cb13004b759acd1e80aea1a8
VladNcode <vldsmrfow@gmail.com>
2021-09-29 12:17:55 +0300

    Intial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c085a8af-72f6-436b-813c-cbeb5cf82b36';
+export const MODAL_CLOSE_SEC = 2.5;
