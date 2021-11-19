3d1b19a1cad35bd6d9208944fa19d2712baa7eda
VladNCode <vldsmrfow@gmail.com>
2021-11-19 18:10:46 +0200

    updated npm packages

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -9,7 +9,7 @@ export default class View {
    * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
    * @returns {undefined | string} A markup string is returned if render=false
    * @this {Object} View instance
-   * @author Vladislav Nikiforov
+   * @author Vladyslav Nikiforov
    * @todo Finish implementation
    */
   render(data, render = true) {
